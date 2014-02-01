c371f44bde31c0d061e98cd8807f10916e423c85
nelsonic <nodecoder@gmail.com>
2014-02-01 18:11:30 +0000

    completed https://www.dashingd3js.com/using-the-svg-coordinate-space

--- demo/script.js
+++ demo/script.js
@@ -1,22 +1,23 @@
-var circleRadii = [40, 20, 10];
+var spaceCircles = [30, 70, 110];
 
 var svgContainer = d3.select("body").append("svg")
-.attr("width", 600)
-.attr("height", 100);
+	.attr("width", 200)
+	.attr("height", 200);
+	// .style("border","1px solid black");
 
 var circles = svgContainer.selectAll("circle")
-.data(circleRadii)
+.data(spaceCircles)
 .enter()
 .append("circle");
 
 var circleAttributes = circles
-.attr("cx", 50)
-.attr("cy", 50)
-.attr("r", function (d) { return d; })
+.attr("cx", function (d) { return d; })
+.attr("cy", function (d) { return d; })
+.attr("r", 20)
 .style("fill", function(d){
 	var returnColor;
-	if (d===40){ returnColor = "green"; }
-	if (d===20){ returnColor = "blue"; }
-	if (d===10){ returnColor = "red";}
+	if (d===30){ returnColor = "green"; }
+	if (d===70){ returnColor = "blue"; }
+	if (d===110){ returnColor = "red";}
 	return returnColor;
 });
