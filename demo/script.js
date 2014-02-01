ae6844c56270a036ba40845eab11ba9725c16615
nelsonic <nodecoder@gmail.com>
2014-02-01 14:30:19 +0000

    completed the 3 circles svg tutorial

--- demo/script.js
+++ demo/script.js
@@ -1,7 +1,22 @@
-var theData = [ 1, 2, 3 ];
+var circleRadii = [40, 20, 10];
 
-var p = d3.select("body").selectAll("p")
-.data(theData)
+var svgContainer = d3.select("body").append("svg")
+.attr("width", 600)
+.attr("height", 100);
+
+var circles = svgContainer.selectAll("circle")
+.data(circleRadii)
 .enter()
-.append("p")
-.text( function (d,i) {	return "i = " + i + " d = "+d; } );
+.append("circle");
+
+var circleAttributes = circles
+.attr("cx", 50)
+.attr("cy", 50)
+.attr("r", function (d) { return d; })
+.style("fill", function(d){
+	var returnColor;
+	if (d===40){ returnColor = "green"; }
+	if (d===20){ returnColor = "blue"; }
+	if (d===10){ returnColor = "red";}
+	return returnColor;
+});
