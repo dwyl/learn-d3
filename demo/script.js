6b65a886df0a3f1a27c46842e3521ad2cb18477b
nelsonic <nodecoder@gmail.com>
2014-02-02 23:10:52 +0000

    completed DashingD3JS section on JSON: https://www.dashingd3js.com/using-json-to-simplify-code

--- demo/script.js
+++ demo/script.js
@@ -1,23 +1,19 @@
-var spaceCircles = [30, 70, 110];
+var jsonCircles = [
+	{ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
+	{ "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
+	{ "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
 
 var svgContainer = d3.select("body").append("svg")
 	.attr("width", 200)
 	.attr("height", 200);
-	// .style("border","1px solid black");
 
 var circles = svgContainer.selectAll("circle")
-.data(spaceCircles)
-.enter()
-.append("circle");
+	.data(jsonCircles)
+	.enter()
+	.append("circle");
 
 var circleAttributes = circles
-.attr("cx", function (d) { return d; })
-.attr("cy", function (d) { return d; })
-.attr("r", 20)
-.style("fill", function(d){
-	var returnColor;
-	if (d===30){ returnColor = "green"; }
-	if (d===70){ returnColor = "blue"; }
-	if (d===110){ returnColor = "red";}
-	return returnColor;
-});
+.attr("cx", function (d) { return d.x_axis; })
+.attr("cy", function (d) { return d.y_axis; })
+.attr("r", function (d) { return d.radius; })
+.style("fill", function(d){ return d.color; });
