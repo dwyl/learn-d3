cf1dd605f362df0a107668c6fdd649e15d2a5c5e
nelsonic <nodecoder@gmail.com>
2014-02-04 07:51:49 +0000

    grouped and transformed circles. WoW! :-P

--- demo/script.js
+++ demo/script.js
@@ -1,20 +1,40 @@
-var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
-	{ "x": 40,  "y": 10}, { "x": 60,  "y": 40},
-	{ "x": 80,  "y": 5},  { "x": 100, "y": 60} ];
+ var circleData = [
+	{ "cx": 20, "cy": 20, "radius": 20, "color" : "green" },
+	{ "cx": 70, "cy": 70, "radius": 20, "color" : "purple" }];
+ 
+ var rectangleData = [
+	{ "rx": 110, "ry": 110, "height": 30, "width": 30, "color" : "blue" },
+	{ "rx": 160, "ry": 160, "height": 30, "width": 30, "color" : "red" }];
 
 var svgContainer = d3.select("body").append("svg")
-	.attr("width", 200)
-	.attr("height", 200);
+	.attr("width",200)
+	.attr("height",200);
 
-var circleGroup = svgContainer.append("g");
+// add SVG Group and transform
+var circleGroup = svgContainer.append("g")
+	.attr("transform", "translate(80,0)");
 
-var lineFunction = d3.svg.line()
-	.x(function(d) { return d.x; })
-	.y(function(d) { return d.y; })
-	.interpolate("linear");
+// add circles to the circleGroup
+var circles = circleGroup.selectAll("circle")
+	.data(circleData)
+	.enter()
+	.append("circle");
 
-var lineGraph = svgContainer.append("path")
-	.attr("d", lineFunction(lineData))
-	.attr("stroke", "blue")
-	.attr("stroke-width", 2)
-	.attr("fill", "none");
+var circleAttributes = circles
+	.attr("cx", function (d) { return d.cx; })
+	.attr("cy", function (d) { return d.cy; })
+	.attr("r", function (d) { return d.radius; })
+	.style("fill", function (d) { return d.color; });
+
+// Rectangles added to the svgContainer
+var rectangles = svgContainer.selectAll("rect")
+	.data(rectangleData)
+	.enter()
+	.append("rect");
+
+var rectangleAttributes = rectangles
+	.attr("x", function (d) { return d.rx; })
+	.attr("y", function (d) { return d.ry; })
+	.attr("height", function (d) { return d.height; })
+	.attr("width", function (d) { return d.width; })
+	.style("fill", function(d) { return d.color; });
