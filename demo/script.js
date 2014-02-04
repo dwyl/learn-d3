a0326e76d62ff4ca6db1c4e4681694d433207804
nelsonic <nodecoder@gmail.com>
2014-02-04 07:30:13 +0000

    learned how to group and transform SVG elements - https://www.dashingd3js.com/svg-group-element-and-d3js

--- demo/script.js
+++ demo/script.js
@@ -1,19 +1,20 @@
-var jsonCircles = [
-	{ "x_axis": 30, "y_axis": 30, "radius": 20, "color" : "green" },
-	{ "x_axis": 70, "y_axis": 70, "radius": 20, "color" : "purple"},
-	{ "x_axis": 110, "y_axis": 100, "radius": 20, "color" : "red"}];
+var lineData = [ { "x": 1,   "y": 5},  { "x": 20,  "y": 20},
+	{ "x": 40,  "y": 10}, { "x": 60,  "y": 40},
+	{ "x": 80,  "y": 5},  { "x": 100, "y": 60} ];
 
 var svgContainer = d3.select("body").append("svg")
 	.attr("width", 200)
 	.attr("height", 200);
 
-var circles = svgContainer.selectAll("circle")
-	.data(jsonCircles)
-	.enter()
-	.append("circle");
+var circleGroup = svgContainer.append("g");
 
-var circleAttributes = circles
-.attr("cx", function (d) { return d.x_axis; })
-.attr("cy", function (d) { return d.y_axis; })
-.attr("r", function (d) { return d.radius; })
-.style("fill", function(d){ return d.color; });
+var lineFunction = d3.svg.line()
+	.x(function(d) { return d.x; })
+	.y(function(d) { return d.y; })
+	.interpolate("linear");
+
+var lineGraph = svgContainer.append("path")
+	.attr("d", lineFunction(lineData))
+	.attr("stroke", "blue")
+	.attr("stroke-width", 2)
+	.attr("fill", "none");
